916f5b939ff5faa8cbb762067288985a0eb71e72
Chad OBrien <obrien.music@gmail.com>
2024-09-18 23:12:00 -0400

    build actual app

---
+++ main.ts
@@ -0,0 +1,54 @@
+const kv = await Deno.openKv()
+
+const validateEmail = (email: string) => {
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+	return emailRegex.test(email) ? email : null
+}
+
+const endpoint = async (request: Request) => {
+	// If request method is not GET or POST, return 405
+	if (!["GET", "POST"].includes(request.method))
+		return new Response("Method not allowed", { status: 405 })
+
+	// If GET request, return all feedback from KV
+	if (request.method === "GET") {
+		try {
+			const feedbackArray = []
+			const feedback = kv.list({ prefix: ["feedback"] })
+			for await (const entry of feedback) {
+				feedbackArray.push(entry.value)
+			}
+	
+			return new Response(JSON.stringify(feedbackArray), {
+				headers: { "content-type": "application/json" }
+			})
+		} catch (error) {
+			console.error(error)
+			return new Response("Internal server error", { status: 500 })
+		}
+	}
+
+	// If POST request, parse the body, validate, and store the feedback
+	try {
+		const body = await request.json()
+
+		if (!body?.message) return new Response("Message is required", { status: 400 })
+
+		const message = body.message
+		const email = validateEmail(body?.email) || "anonymous"
+
+		const feedback = {
+			email,
+			message,
+			dateSubmitted: new Date().toISOString()
+		}
+
+		await kv.set(["feedback", email], feedback)
+		return new Response("Feedback recieved", { status: 200 })
+	} catch (error) {
+		console.error(error)
+		return new Response("Internal server error", { status: 500 })
+	}
+}
+
+Deno.serve(endpoint)
